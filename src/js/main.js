4b1bf10bb4ba9dbabee437182c341a7d179b84ff
jwilkinsondev <jwilkinsondev@gmail.com>
2017-07-12 12:30:16 -0600

    Added basic page/tab functionality

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+function changeTab(event, tabName){
+    var i;
+    var tabLinks;
+    var tabsContent;
+    var tabLinksCount;
+    var tabsContentCount;
+
+    tabsContent = document.getElementsByClassName("tab-content");
+    tabsContentCount = tabsContent.length;
+    // hide the tabs
+    for(i = 0; i < tabsContentCount; i++){
+        tabsContent[i].style.display = "none";
+    }
+
+    tabLinks = document.getElementsByClassName("tab");
+    tabLinksCount = tabLinks.length;
+    // remove any 'active' classes
+    for(i = 0; i < tabLinksCount; i++){
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+
+    // show the new current tab and content
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
